53b1508b4cd2951a5a9e2db567e82ba092174be2
Michael Muzio <michael.muzio@fn-x.design>
2018-08-08 12:06:11 -0700

    audit with comments and reorganizing index.html, style.css, and custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,5 @@
+// Custom animations with wow and jquery
 (function ($) {
-    
     // Init Wow
     wow = new WOW( {
         animateClass: 'animated',
@@ -35,6 +35,7 @@
     
 })(jQuery);
 
+// Custom Map with flag markers
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 4,
@@ -47,4 +48,114 @@ function initMap() {
       map: map,
       icon: image
     });
-  }
+}
+
+// CAD File Upload Handler
+// 
+//
+// Add Click Listener to CAD File Upload Button 
+document.getElementById("CADbtn").addEventListener("click", function() { 
+    document.getElementById("file").click();
+});
+
+var storageRef = firebase.storage().ref();
+var database = firebase.database();
+
+function handleFileSelect(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    var file = evt.target.files[0];
+
+    var metadata = {
+        'contentType': file.type
+    };
+
+    var user = firebase.auth().currentUser;
+    var name, email, uid; // collect name, email, and user ID;
+
+    if (user != null) {
+        name = user.displayName;
+        email = user.email;
+        //emailVerified = user.emailVerified;
+        //uid = user.uid;  
+        // The user's ID, unique to the Firebase project. Do NOT use
+        // this value to authenticate with your backend server, if
+        // you have one. Use User.getToken() instead.
+    }
+
+    // Write user info to database
+    function writeUserData() {
+        firebase.database().ref('users/' + name).set({
+        email: email
+        });
+    }
+
+    // If user uploads valid CAD file, upload to path CAD/name/file.name
+    if (isCAD(file.name)) {
+    storageRef.child('CAD/' + name + '/' + file.name).put(file, metadata).then(function(snapshot) {
+        console.log('Uploaded', snapshot.totalBytes, 'bytes.');
+        console.log(snapshot.metadata);
+        alert("Upload successful!")
+    }).catch(function(error) {
+        // [START onfailure]
+        alert("Upload failed  (╯°□°）╯︵ ┻━┻")
+        // [END onfailure]
+    });
+    // [END oncomplete]
+    }
+    else {
+        alert("Upload failed  (╯°□°）╯︵ ┻━┻")
+    }
+}
+
+window.onload = function() {
+    document.getElementById('file').addEventListener('change', handleFileSelect, false);
+    document.getElementById('file').disabled = false;
+
+    auth.onAuthStateChanged(function(user) {
+        if (user) {
+        console.log('Anonymous user signed-in.', user);
+        document.getElementById('file').disabled = false;
+        } else {
+        console.log('There was no anonymous session. Creating a new anonymous user.');
+        // Sign the user in anonymously since accessing Storage requires the user to be authorized.
+        auth.signInAnonymously();
+        }
+    });
+}
+
+// Get CAD File Extension
+// 
+// Get Extension
+function getExtension(filename) {
+    var parts = filename.split('.');
+    return parts[parts.length - 1];
+}
+
+// Check that extension is Valid CAD file type
+function isCAD(filename) {
+    var ext = getExtension(filename);
+    switch (ext.toLowerCase()) {
+        case 'step':
+        case 'stp':
+        case 'ste':
+        case 'iges':
+        case 'ige':
+        case 'igs':
+        case 'stl':
+        case 'catpart':
+        case 'dxf':
+        case 'dwg':
+        case 'f3d':
+        case 'prt':
+        case 'obj':
+        case 'sldprt':
+        case 'skp':
+        //etc
+            return true;
+    }
+    return false;
+}
+
+
+
